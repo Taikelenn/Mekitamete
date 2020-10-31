5a0abbadfc1d0942ad8531f7175e141ba5fa8bd4
Taikelenn <taikelenn@kasumata.ee>
2020-11-01 00:08:59 +0100

    Added HTTP responder attributes

---
+++ Mekitamete/Http/Responders/HttpEndpointAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mekitamete.Http.Responders
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class HttpEndpointAttribute : Attribute
+    {
+        public string Endpoint { get; }
+
+        public HttpEndpointAttribute(string endpoint)
+        {
+            if (!endpoint.StartsWith('/'))
+            {
+                endpoint = '/' + endpoint;
+            }
+
+            Endpoint = endpoint;
+        }
+    }
+}
---
+++ Mekitamete/Http/Responders/HttpMethodAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mekitamete.Http.Responders
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public class HttpMethodAttribute : Attribute
+    {
+        public string Method { get; }
+
+        public HttpMethodAttribute(string methodName)
+        {
+            Method = methodName.ToUpperInvariant();
+        }
+    }
+}
