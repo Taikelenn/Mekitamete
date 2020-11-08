1bb10b570c920c501446f3475e6822f0c0b369bb
Taikelenn <taikelenn@kasumata.ee>
2020-11-08 18:55:56 +0100

    Added a specialized exception type for daemon failures

---
+++ Mekitamete/Daemons/CryptoDaemonException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mekitamete.Daemons
+{
+    class CryptoDaemonException : Exception
+    {
+        public CryptoDaemonException()
+        {
+        }
+
+        public CryptoDaemonException(string message) : base(message)
+        {
+        }
+
+        public CryptoDaemonException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
